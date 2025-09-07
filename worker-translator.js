4c5500fd4f180c12282acf840f396914e838647b
picohook <nanochemist@gmail.com>
2025-09-07 12:58:14 +0300

    Update worker-translator.js

--- worker-translator.js
+++ worker-translator.js
@@ -25,38 +25,4 @@ async function handleRequest(request) {
     headers: { 'Content-Type': 'application/json' },
     status: 200
   });
-
-  const url = new URL(request.url);
-  const text = url.searchParams.get("text");
-  const target = url.searchParams.get("target");
-
-  if (!text || !target) {
-    return new Response(JSON.stringify({ error: "Text or target param missing" }), {
-      headers: {
-        'Content-Type': 'application/json',
-        // CORS hatasını çözmek için bu satırı ekleyin:
-        'Access-Control-Allow-Origin': '*' 
-      },
-      status: 400
-    });
-  }
-
-  // Burada Google Translate API'si ile ilgili kodunuz olacak.
-
-  const translatedText = "Çevrilmiş Metin Örneği"; // Örnek metin
-
-  const responseBody = {
-    data: {
-      translations: [{ translatedText: translatedText }]
-    }
-  };
-
-  return new Response(JSON.stringify(responseBody), {
-    headers: {
-      'Content-Type': 'application/json',
-      // CORS hatasını çözmek için bu satırı ekleyin:
-      'Access-Control-Allow-Origin': '*' 
-    },
-    status: 200
-  });
 }
