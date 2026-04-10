d4853095519b088efeb607c73647da5d38c694e7
picohook <nanochemist@gmail.com>
2026-04-10 21:26:50 +0300

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './announcements.html',
+    './admin.html',
+    './profile.html',
+    './privacy.html',
+    './terms.html',
+    './cookies.html',
+    './partials/**/*.html',
+    './assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
